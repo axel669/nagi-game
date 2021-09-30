31166943d4c0b295cf1e4e16a78a4aad2cb3d299
Axel669 <haryuu.hanekata@gmail.com>
2021-09-29 23:48:22 -0700

    update prod build settings

--- config/rollup.production.js
+++ config/rollup.production.js
@@ -9,22 +9,60 @@ import copy from "./plugins/copy.js"
 import appInfo from "./app-info.js"
 import template from "./html-template.js"
 
-export default {
-    input: "./src/main.js",
-    output: {
-        file: `./build/app-${Date.now()}.js`,
-        format: "iife",
+export default [
+    {
+        input: "./src/main.js",
+        output: {
+            file: `./build/app-${Date.now()}.js`,
+            format: "iife",
+        },
+        plugins: [
+            svelte(),
+            resolve(),
+            commonjs(),
+            terser(),
+            html({
+                filename: "./build/index.html",
+                title: appInfo.name,
+                template,
+            }),
+            copy("static", "build")
+        ]
     },
-    plugins: [
-        svelte(),
-        resolve(),
-        commonjs(),
-        terser(),
-        html({
-            filename: "./build/index.html",
-            title: appInfo.name,
-            template,
-        }),
-        copy("static", "build")
-    ]
-}
+    {
+        input: "./src/question/main.js",
+        output: {
+            file: `./build/question/app-${Date.now()}.js`,
+            format: "iife",
+        },
+        plugins: [
+            svelte(),
+            resolve(),
+            commonjs(),
+            terser(),
+            html({
+                filename: "./build/question/index.html",
+                title: appInfo.name,
+                template,
+            }),
+        ]
+    },
+    {
+        input: "./src/chat/main.js",
+        output: {
+            file: `./build/chat/app-${Date.now()}.js`,
+            format: "iife",
+        },
+        plugins: [
+            svelte(),
+            resolve(),
+            commonjs(),
+            terser(),
+            html({
+                filename: "./build/chat/index.html",
+                title: appInfo.name,
+                template,
+            }),
+        ]
+    },
+]
